7c8581ff800aad940995722976c400d8e6baaa4c
Yoshinori Tahara <read.eval.print@gmail.com>
2009-08-15 01:41:22 +0900

    Direct threadede code はこんなこんなのでいい？

--- chocoforth.asm
+++ chocoforth.asm
@@ -2,39 +2,13 @@
 ;; nasm -f elf64 chocoforth.asm
 ;; ld -s -o chocoforth chocoforth.o
 ;; ./chocoforth
-
-        CELLL   EQU     8
-
-%macro PUSHRSP 1
-        lea     rbp,    [rbp - CELLL]
-        mov     rbp,    %1
-%endmacro
-
-%macro POPRSP 1
-        mov     %1,     [rbp]
-        lea     rbp,    [rbp + CELLL]
-%endmacro
-
-%macro NEXT 0
-	lodsq
-	jmp rax
-%endmacro
-
-section .text
-
-align   8
-
-DOCOL:
-        PUSHRSP rsi
-        add     rax,    CELLL
-        mov     rsi,    rax
-        NEXT
-
+;;
+;;
 ;; <<Direct Threaded Code>>
 ;; DOUBLE:
 ;;         PUSHRSP rsi
-;;         mov     rsi,    DOUBLE_code+8
-;;         jmp     [DOUBLE_code]
+;;         mov     rsi,    DOUBLE_code
+;;         NEXT
 ;; DOUBLE_code:
 ;;         DUP
 ;;         PLUS
@@ -46,7 +20,7 @@ DOCOL:
 ;; EXIT:
 ;;         POPRSP  rsi
 ;;         NEXT
-;; 
+;;
 ;; <<Indirect Threaded Code>>
 ;; DOUBLE:
 ;;         DOCOL                   ; codeword
@@ -64,7 +38,37 @@ DOCOL:
 ;; EXIT_code:
 ;;         POPRSP  rsi
 ;;         NEXT
+;; DOCOL:
+;;         PUSHRSP rsi
+;;         add     rax,    CELLL
+;;         mov     rsi,    rax
+;;         NEXT
 
+        CELLL   EQU     8
+
+%macro PUSHRSP 1
+        lea     rbp,    [rbp - CELLL]
+        mov     [rbp],  %1
+%endmacro
+
+%macro POPRSP 1
+        mov     %1,     [rbp]
+        lea     rbp,    [rbp + CELLL]
+%endmacro
+
+%macro NEXT 0
+	lodsq
+	jmp rax
+%endmacro
+
+section .text
+
+align   8
+
+EXIT:
+        POPRSP  rsi
+        NEXT
+        
 
 message:
         push    msg
@@ -81,24 +85,65 @@ say:
         mov     rax,    1       ; 出力システムコール
         mov     rdi,    1       ; 標準出力
         syscall                 ; システムコール実行
-        mov     rsi,    r12     ; ris を復元
+        mov     rsi,    r12     ; rsi を復元
         NEXT
+
+hello:
+        PUSHRSP rsi
+        mov     rsi,    hello_code + CELLL
+        jmp     [hello_code]
+section .data
+align 8
+hello_code:
+        dq      message
+        dq      say
+        dq      EXIT
+section .text
+
+double_hello:
+        PUSHRSP rsi
+        mov     rsi,    double_hello_code + CELLL
+        jmp     [double_hello_code]
+section .data
+align 8
+double_hello_code:
+        dq      hello
+        dq      hello
+        dq      EXIT
+section .text
+
 
 global _start
 
 _start:
-        mov     rsi,    hello
+	mov     rbp,    return_stack_top ; Initialise the return stack.
+        mov     rsi,    entry_point
         NEXT
 
 section .data
 
 msg:
-        db 'Hello World!', 0ah
+        db 'Hello World!', 20h
         db 'まみむめも♪', 0ah
 	len equ $ -msg
 
 align 8
-hello:
-        dq      message
-        dq      say
+entry_point:
+        dq      double_hello
         dq      system_exit
+
+
+	RETURN_STACK_SIZE       EQU     8192
+	BUFFER_SIZE             EQU     4096
+
+section .bss
+;; FORTH return stack.
+align 4096
+return_stack:
+	resb    RETURN_STACK_SIZE
+return_stack_top:               ; Initial top of return stack.
+
+;;  This is used as a temporary input buffer when reading from files or the terminal.
+align 4096
+buffer:
+        resb    BUFFER_SIZE
